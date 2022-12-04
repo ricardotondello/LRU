550e2ef2df160e28bae7f5ddde0a46514b3ad3bf
Ricardo Tondello <rkdtondello@gmail.com>
2022-12-04 11:01:52 +0100

    performance boost

---
+++ LRU/Helpers/LinkedNode.cs
@@ -0,0 +1,10 @@
+namespace LRU.Helpers
+{
+    internal class LinkedNode<TKey, TValue>
+    {
+        public LinkedNode<TKey, TValue> Next { get; set; }
+        public LinkedNode<TKey, TValue> Prev { get; set; }
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+    }
+}
