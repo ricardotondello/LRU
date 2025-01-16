a2ede1f56867ad767db8a3c4a0fddefc02708329
Ricardo Tondello <rkdtondello@gmail.com>
2025-01-16 12:43:03 +0100

    upgrade libs

--- LRU.Tests/LeastRecentlyUsedCacheTests.cs
+++ LRU.Tests/LeastRecentlyUsedCacheTests.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
-using FluentAssertions;
 using Xunit;
 
 [assembly: ExcludeFromCodeCoverage]
@@ -20,10 +19,8 @@ public class LeastRecentlyUsedCacheTests
     [InlineData(0)]
     public void Should_Return_ArgumentException_When_Capacity_Is_Invalid(int capacity)
     {
-        var act = () => new LeastRecentlyUsedCache<int, int>(capacity);
-
-        act.Should().Throw<ArgumentException>()
-            .WithMessage("Must be greater than 0. (Parameter 'capacity')");
+        var ex = Assert.Throws<ArgumentException>(() => new LeastRecentlyUsedCache<int, int>(capacity));
+        Assert.Equal("Must be greater than 0. (Parameter 'capacity')", ex.Message);
     }
 
     [Theory]
@@ -41,11 +38,11 @@ public class LeastRecentlyUsedCacheTests
 
         cache.TryGetValue(capacity, out var validPosition);
 
-        validPosition.Should().BeGreaterThan(0);
+        Assert.True(validPosition > 0);
 
         var result = cache.TryGetValue(capacity + 1, out _);
 
-        result.Should().BeFalse();
+        Assert.False(result);
     }
 
     [Theory]
@@ -61,7 +58,7 @@ public class LeastRecentlyUsedCacheTests
 
         cache.TryGetValue(key, out var result);
 
-        result.Should().Be(expected);
+        Assert.Equal(expected, result);
     }
 
     [Theory]
@@ -74,67 +71,52 @@ public class LeastRecentlyUsedCacheTests
 
         var result = cache.TryGetValue(2, out _);
 
-        result.Should().BeFalse();
+        Assert.False(result);
 
         var result2 = cache.TryGetValue(1, out _);
-        result2.Should().BeTrue();
+        Assert.True(result2);
     }
 
     [Theory]
     [InlineData(2)]
     public void Should_Contains_Item(int capacity)
     {
-        var cache = new LeastRecentlyUsedCache<int, int>(capacity)
-        {
-            { 1, 1 },
-            { 2, 2 },
-            { 3, 3 },
-            { 4, 4 }
-        };
-
-        cache.Contains(new KeyValuePair<int, int>(3, 3)).Should().BeTrue();
-        cache.ContainsKey(4).Should().BeTrue();
-        cache.ContainsKey(2).Should().BeFalse();
-        cache.Contains(new KeyValuePair<int, int>(5, 5)).Should().BeFalse();
+        var cache = new LeastRecentlyUsedCache<int, int>(capacity) { { 1, 1 }, { 2, 2 }, { 3, 3 }, { 4, 4 } };
+        
+        var contains = cache.Contains(new KeyValuePair<int, int>(3, 3));
+        var notContains = cache.Contains(new KeyValuePair<int, int>(5, 5));
+        
+        Assert.True(contains);
+        Assert.True(cache.ContainsKey(4));
+        Assert.False(cache.ContainsKey(2));
+        Assert.False(notContains);
     }
 
     [Theory]
     [InlineData(4)]
     public void Should_Clear_Items_From_Cache(int capacity)
     {
-        var cache = new LeastRecentlyUsedCache<int, int>(capacity)
-        {
-            { 1, 1 },
-            { 2, 2 },
-            { 3, 3 },
-            { 4, 4 }
-        };
+        var cache = new LeastRecentlyUsedCache<int, int>(capacity) { { 1, 1 }, { 2, 2 }, { 3, 3 }, { 4, 4 } };
 
-        cache.Count.Should().Be(4);
+        Assert.Equal(4, cache.Count);
         cache.Clear();
-        cache.Count.Should().Be(0);
+        Assert.Empty(cache);
     }
 
     [Theory]
     [InlineData(4)]
     public void Should_GetEnumerator_Items(int capacity)
     {
-        var cache = new LeastRecentlyUsedCache<int, int>(capacity)
-        {
-            { 1, 9 },
-            { 2, 8 },
-            { 3, 7 },
-            { 4, 6 }
-        };
+        var cache = new LeastRecentlyUsedCache<int, int>(capacity) { { 1, 9 }, { 2, 8 }, { 3, 7 }, { 4, 6 } };
 
         var enumeratorValue = cache.GetEnumerator();
         enumeratorValue.MoveNext();
-        enumeratorValue.Current.Key.Should().Be(1);
-        enumeratorValue.Current.Value.Should().Be(9);
+        Assert.Equal(1, enumeratorValue.Current.Key);
+        Assert.Equal(9, enumeratorValue.Current.Value);
 
         enumeratorValue.MoveNext();
-        enumeratorValue.Current.Key.Should().Be(2);
-        enumeratorValue.Current.Value.Should().Be(8);
+        Assert.Equal(2, enumeratorValue.Current.Key);
+        Assert.Equal(8, enumeratorValue.Current.Value);
         enumeratorValue.Dispose();
     }
 
@@ -145,9 +127,12 @@ public class LeastRecentlyUsedCacheTests
         if (cache == null) throw new ArgumentNullException(nameof(cache));
 
         var enumerableCache = AsWeakEnumerable(cache);
-        var result = enumerableCache.Cast<KeyValuePair<int, int>>().Take(1).ToArray();
-        result.GetEnumerator().MoveNext();
-        result.GetEnumerator().Should().NotBeNull();
+        var result = enumerableCache.Cast<KeyValuePair<int, int>>()
+            .Take(1)
+            .ToArray();
+        result.GetEnumerator()
+            .MoveNext();
+        Assert.NotNull(result.GetEnumerator());
         return;
 
         IEnumerable AsWeakEnumerable(IEnumerable source)
@@ -162,14 +147,9 @@ public class LeastRecentlyUsedCacheTests
     [Fact]
     public void Should_Throw_ArgumentNullException_When_CopyTo_Is_Called_With_Null_Argument()
     {
-        var cache = new LeastRecentlyUsedCache<int, int>(1)
-        {
-            { 1, 9 }
-        };
+        var cache = new LeastRecentlyUsedCache<int, int>(1) { { 1, 9 } };
 
-        var act = () => cache.CopyTo(null!, 0);
-
-        act.Should().Throw<ArgumentNullException>();
+        Assert.Throws<ArgumentNullException>(() => cache.CopyTo(null!, 0));
     }
 
     [Theory]
@@ -177,85 +157,64 @@ public class LeastRecentlyUsedCacheTests
     [InlineData(2)]
     public void Should_Throw_ArgumentOutOfRangeException_When_CopyTo_Is_Called_With_Invalid_IndexArray(int invalidIndex)
     {
-        var cache = new LeastRecentlyUsedCache<int, int>(1)
-        {
-            { 1, 9 }
-        };
+        var cache = new LeastRecentlyUsedCache<int, int>(1) { { 1, 9 } };
 
-        var arrayResult = new[]
-        {
-            new KeyValuePair<int, int>(1, 2)
-        };
+        var arrayResult = new[] { new KeyValuePair<int, int>(1, 2) };
 
-        var act = () => cache.CopyTo(arrayResult, invalidIndex);
-
-        act.Should().Throw<ArgumentOutOfRangeException>();
+        Assert.Throws<ArgumentOutOfRangeException>(() => cache.CopyTo(arrayResult, invalidIndex));
     }
 
     [Theory]
     [InlineData(0)]
     public void Should_Throw_ArgumentException_When_CopyTo_Items_Does_Not_Fit_In_Array(int arrayIndex)
     {
-        var cache = new LeastRecentlyUsedCache<int, int>(4)
-        {
-            { 1, 9 },
-            { 2, 9 },
-            { 3, 9 },
-            { 4, 9 }
-        };
+        var cache = new LeastRecentlyUsedCache<int, int>(4) { { 1, 9 }, { 2, 9 }, { 3, 9 }, { 4, 9 } };
 
-        var arrayResult = new[]
-        {
-            new KeyValuePair<int, int>(1, 2)
-        };
+        var arrayResult = new[] { new KeyValuePair<int, int>(1, 2) };
 
-        var act = () => cache.CopyTo(arrayResult, arrayIndex);
-
-        act.Should().Throw<ArgumentException>()
-            .WithMessage("Not enough elements after arrayIndex in the destination array.");
+        var ex = Assert.Throws<ArgumentException>(() => cache.CopyTo(arrayResult, arrayIndex));
+        Assert.Equal("Not enough elements after arrayIndex in the destination array.", ex.Message);
     }
 
     [Theory]
     [InlineData(4)]
     public void Should_CopyTo_Array(int capacity)
     {
-        var cache = new LeastRecentlyUsedCache<int, int>(capacity)
-        {
-            { 1, 9 },
-            { 2, 8 },
-            { 3, 7 },
-            { 4, 6 }
-        };
+        var cache = new LeastRecentlyUsedCache<int, int>(capacity) { { 1, 9 }, { 2, 8 }, { 3, 7 }, { 4, 6 } };
 
         var arrayResult = new KeyValuePair<int, int>[4];
         cache.CopyTo(arrayResult, 0);
 
-        arrayResult.All(x => cache.ContainsKey(x.Key) && cache.Values.Contains(x.Value)).Should().BeTrue();
+        Assert.True(arrayResult.All(x => cache.ContainsKey(x.Key) && cache.Values.Contains(x.Value)));
     }
 
     [Fact]
     public void Should_IsReadOnly_Be_False()
     {
-        var cache = new LeastRecentlyUsedCache<int, int>(1) { new(1, 1) };
-        if (cache == null) throw new ArgumentNullException(nameof(cache));
-        cache.IsReadOnly.Should().BeFalse();
+        var cache = new LeastRecentlyUsedCache<int, int>(1) { new KeyValuePair<int, int>(1, 1) };
+        if (cache == null)
+        {
+            throw new ArgumentNullException(nameof(cache));
+        }
+
+        Assert.False(cache.IsReadOnly);
     }
 
     [Fact]
     public void Should_ReturnValues_When_Get_Or_Set_The_Element_With_Specified_Key()
     {
-        var cache = new LeastRecentlyUsedCache<int, int>(1) { new(1, 1) };
+        var cache = new LeastRecentlyUsedCache<int, int>(1) { new KeyValuePair<int, int>(1, 1) };
 
         cache[1] = 2;
-        cache[1].Should().Be(2);
+        Assert.Equal(2, cache[1]);
     }
 
     [Fact]
     public void Should_ReturnDefaultValue_When_Get_The_Element_Is_Not_Present()
     {
-        var cache = new LeastRecentlyUsedCache<int, int>(1) { new(1, 1) };
+        var cache = new LeastRecentlyUsedCache<int, int>(1) { new KeyValuePair<int, int>(1, 1) };
 
-        cache[10].Should().Be(default);
+        Assert.Equal(0, cache[10]);
     }
 
     [Theory]
@@ -265,7 +224,7 @@ public class LeastRecentlyUsedCacheTests
         var cache = new LeastRecentlyUsedCache<int, decimal>(capacity) { { 1, 1 }, { 2, 2 } };
         var key = new KeyValuePair<int, decimal>(3, 3m);
         var result = cache.Remove(key);
-        result.Should().BeFalse();
+        Assert.False(result);
     }
 
     [Fact]
@@ -278,18 +237,17 @@ public class LeastRecentlyUsedCacheTests
 
         var tasks = Enumerable.Range(1, 100)
             .Select(_ => Task.Run(() =>
-                {
-                    var r = new Random();
-                    cache.Add(key, Set(r.Next(1, int.MaxValue)));
-                    return Task.CompletedTask;
-                }
-            ))
+            {
+                var r = new Random();
+                cache.Add(key, Set(r.Next(1, int.MaxValue)));
+                return Task.CompletedTask;
+            }))
             .ToArray();
 
         await Task.WhenAll(tasks);
 
-        cache.TryGetValue(key, out var result).Should().BeTrue();
-        result.Should().Be(resultForTestingThreadSafe);
+        Assert.True(cache.TryGetValue(key, out var result));
+        Assert.Equal(resultForTestingThreadSafe, result);
         return;
 
         int Set(int i)
@@ -311,38 +269,20 @@ public class LeastRecentlyUsedCacheTests
     [Fact]
     public void Should_ReturnKeys_When_Get_Keys()
     {
-        var cache = new LeastRecentlyUsedCache<int, int>(1) { new(1, 1) };
+        var cache = new LeastRecentlyUsedCache<int, int>(1) { new KeyValuePair<int, int>(1, 1) };
 
-        cache.Keys.Should().BeEquivalentTo(new List<int> { 1 });
+        Assert.Equivalent(new List<int> { 1 }, cache.Keys);
     }
 
-    public class LruTestData : TheoryData<int, int[,], int, int>
+    private class LruTestData : TheoryData<int, int[,], int, int>
     {
         public LruTestData()
         {
-            Add(
-                2, new[,]
-                {
-                    { 1, 1 }, { 2, 2 }, { 3, 3 }, { 4, 4 }
-                },
-                3, 3
-            );
+            Add(2, new[,] { { 1, 1 }, { 2, 2 }, { 3, 3 }, { 4, 4 } }, 3, 3);
 
-            Add(
-                50, new[,]
-                {
-                    { 10, 90 }, { 20, 77 }, { 33, 5 }, { 35, 44 }, { 45, 89 }
-                },
-                45, 89
-            );
+            Add(50, new[,] { { 10, 90 }, { 20, 77 }, { 33, 5 }, { 35, 44 }, { 45, 89 } }, 45, 89);
             //repeated data test
-            Add(
-                2, new[,]
-                {
-                    { 1, 1 }, { 2, 2 }, { 3, 3 }, { 4, 4 }, { 4, 4 }, { 4, 4 }, { 4, 4 }, { 4, 4 }
-                },
-                3, 3
-            );
+            Add(2, new[,] { { 1, 1 }, { 2, 2 }, { 3, 3 }, { 4, 4 }, { 4, 4 }, { 4, 4 }, { 4, 4 }, { 4, 4 } }, 3, 3);
         }
     }
 }
